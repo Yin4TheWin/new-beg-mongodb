0a9217c0919b6da7193cc476b4cce34583c74815
Franklin Yin <yin4thewin@gmail.com>
2021-03-24 00:50:52 -0400

    Add better documentation

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ const client = new MongoClient(uri, {
 let db, cursor
 
 let test=async()=>{
+    console.log("Starting...")
     try {
     let identifiers=""
+    //Connect to MongoDB
     await client.connect();
+    console.log("Connected to MongoDB")
     db = client.db("employment")
     cursor = db.collection('jobInfo').find({});
+    //Create a string of identifiers, which are just every field for a job listing combined together.
+    //Used to keep track of and remove duplicates.
     await cursor.forEach((info)=>{
         identifiers+="IDSTART"+info.name+info.field+info.time+info.link+info.address+info.phone+info.exp+info.education+info.major+"IDEND"
     })
-    console.log(identifiers[0])
+    //Sign into and load Google Sheets info
     await doc.useServiceAccountAuth(require('./credential.json'))
     await doc.loadInfo()
+    console.log("Connected to Google Sheets")
     const rows=await doc.sheetsByIndex[0].getRows()
     let toPush=[]
     rows.forEach(async (el)=>{
-        //Name Field Commitment Link Address Phone Experience Education Major
+        //Check if any field is null, then replace.
         if(el.Name==null) el.Name="None listed"
         if(el.Field==null) el.Field="None listed"
         if(el.Commitment==null) el.Commitment="None listed"
@@ -37,7 +43,7 @@ let test=async()=>{
         if(el.Major==null) el.Major="None listed"
         let id="IDSTART"+el.Name+el.Field+el.Commitment+el.Link+el.Address+el.Phone+el.Experience+el.Education+el.Major+"IDEND"
         if(!identifiers.includes(id)){
-            //name field time link address phone exp education major
+            //Only push unique entries.
             toPush.push({
                 name: el.Name,
                 field: el.Field,
@@ -51,11 +57,14 @@ let test=async()=>{
             })
         }
     })
-    console.log(doc.title)
-    toPush.forEach(el=>{
-        console.log(el)
-    })
-    await db.collection('jobInfo').insertMany(toPush)
+    if(toPush.length>0){
+        await db.collection('jobInfo').insertMany(toPush)
+        console.log("Added:\n")
+        toPush.forEach(el=>{
+            console.log(el)
+        })
+    } else
+        console.log("Nothing new to add!")
     } catch (e) {
         console.error(e);
     } finally {
